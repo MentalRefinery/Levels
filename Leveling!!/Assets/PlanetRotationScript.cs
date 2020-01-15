144572726c8f9588c562544b6aeca536c8e27651
Mental Refinery <mentalrefinery@gmail.com>
2020-01-15 22:53:19 +0200

    Kepler orbits + spaceship added, Working on planets

---
+++ Leveling!!/Assets/PlanetRotationScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotationScript : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
